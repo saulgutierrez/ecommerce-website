00426d22ad3947f948cae94739851e04194301e1
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-02-23 21:34:01 -0600

    Estilizamos los formularios de incio de sesión y registro de la aplicación. Aún no finaliizado.

--- src/views/login.php
+++ src/views/login.php
@@ -5,15 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../../public/svg/logo.svg" type="image/x-icon">
     <script src="../../public/js/jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="../../public/css/login.css">
     <title>Login</title>
 </head>
 <body>
+    <a class="logo" href="../../index.php">
+        <div class="imgBox">
+            <img src="../../public/svg/logo.svg" alt="">
+        </div>
+        <div>eShop</div>
+    </a>
     <form action="login-password.php" method="POST">
+        <label for="">Iniciar sesión</label>
         <label for="email">Direccion de email o numero de teléfono móvil</label>
         <input type="text" name="email">
         <input type="submit" value="Continuar" class="email-button"></input>
+        <details>
+            <summary>¿Necesitas ayuda?</summary>
+            <p>¿Olvidaste tu contraseña</p>
+            <p>Otros problemas para iniciar sesión</p>
+        </details>
         <div class="message" id="message"></div>
     </form>
+    <p>¿Eres nuevo en eShop?</p>
+    <a href="sign-up.php">Crear tu cuenta en eShop</a>
     <script src="../handlers/check-full-fields.js"></script>
 </body>
 </html>
--- src/views/sign-up.php
+++ src/views/sign-up.php
@@ -5,9 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../../public/svg/logo.svg" type="image/x-icon">
     <script src="../../public/js/jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="../../public/css/sign-up.css">
     <title>Sign up</title>
 </head>
 <body>
+    <a class="logo" href="../../index.php">
+        <div class="imgBox">
+            <img src="../../public/svg/logo.svg" alt="">
+        </div>
+        <div>eShop</div>
+    </a>
     <form method="POST">
         <label for="name">Nombre</label>
         <input type="text" name="name" placeholder="Nombre y apellido">
@@ -19,6 +26,8 @@
         <input type="password" name="password">
         <input type="submit" value="Continuar" class="email-button"></input>
         <div class="message" id="message"></div>
+        <hr>
+        <p>¿Ya tienes una cuenta? <a href="login.php">Iniciar sesión</a></p>
     </form>
     <script src="../handlers/sign-up.js"></script>
 </body>
